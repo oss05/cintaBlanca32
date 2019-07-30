27f61b4dbdf33f55d6418de5432d09ed0f9cb817
Luis Osvaldo <aldolp40@gmail.com>
2019-07-29 19:11:30 -0500

    intro js

---
+++ 03_JavaScript/main.js
@@ -0,0 +1,37 @@
+// alert("Hola, si funcione :D")
+
+
+//tipos de variables
+//valor numerico
+let numerico = 10
+//valor string
+let texto = "Soy un texto"
+//valor booleano
+let boolean = true
+//arreglos
+let array = []
+// objetos
+let object = {}
+
+let suma = numerico + 10
+
+// console.log(suma)
+// console.log("Hola como estas?")
+// console.log(numerico + 10)
+
+// let saludoNombre = 
+
+// let nombre = prompt("Como te llamas?")
+// console.log("nombre")
+// console.log(nombre)
+// console.log("Hola" + " " + nombre)
+
+let nombre = prompt("como te llamas")
+let correo = prompt("pon tu correo")
+let celular = prompt("pasa tu cel")
+
+console.log(nombre)
+console.log(correo)
+console.log(celular)
+
+console.log("Mi usuario es " + nombre + " con el correo " + correo + " y su celular es " + celular)
