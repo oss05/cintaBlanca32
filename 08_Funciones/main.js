deecff8c38c3ae47024cd5b64e1932caf1cb26dc
Kelebra28 <ricardokelebra@gmail.com>
2019-08-01 10:31:23 -0500

    Terminamos ciclos/ Empezamos funciiones

---
+++ 08_Funciones/main.js
@@ -0,0 +1,28 @@
+//Funciones
+
+// function suma () {
+
+//    console.log( 10 + 10)
+
+// }
+
+// suma()
+// console.log(suma())
+
+// const suma = function (){
+
+// }
+
+// const suma = () => {
+//     console.log( 10 + 10)
+// }
+
+// suma()
+const suma = (a=2,b=9) => {
+    console.log( a + b)
+}
+
+suma(10,5)
+suma(100,50)
+suma("Hola", " xd")
+suma()
